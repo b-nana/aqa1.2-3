f8b16ca505ed1c7c392954cdfd624361b563b3ac
b-nana <babenkoaa97@gmail.com>
2022-09-23 15:54:51 +0300

    Test failed

--- src/test/java/ru/netology/service/Test.java
+++ src/test/java/ru/netology/service/Test.java
@@ -19,7 +19,7 @@ class TestV1 {
         // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"));
+                .body("data", equalTo("other data"));
         ;
     }
 }
